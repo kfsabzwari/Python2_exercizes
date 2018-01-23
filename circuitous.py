ff2e04843896b7ead848b0572456d39f26c1f2c8
Khalid Sabzwari (ksabzwar) <ksabzwar@cisco.com>
2018-01-23 12:07:11 -0800

    2nd commit

--- circuitous.py
+++ circuitous.py
@@ -9,16 +9,19 @@ circle management tasks to make the world a better place.
 import math
 from collections import namedtuple
 
-Version = namedtuple('Version',['major', 'minor', 'mirco'])
+Version = namedtuple('Version', ['major', 'minor', 'micro'])
 
 class Circle(object):
-    ''' Create a circle instance from a radius
-    As part of an xxx
-    '''
+    """Create a circle instance from a radius.
+
+    As part of an advanced circle analytics toolkit.
+
+    """
+    version = Version(0, 1, 0)
     
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        "Program quadrant  xxx"
+        "Perform quadrature of a planar shape of uniform revolution."
         return math.pi * self.radius**2.0
--- client_code.py
+++ client_code.py
@@ -1,11 +1,39 @@
-"Show-off the Circuitous code from the user's point of view"
+"Show-off the Circuitous code from the user's point of view."
 
 from circuitous import Circle
 
-print u'Tutorial from Circiutous\N{trade mark sign}'
-print 'Circle class version: %d.%d' % Circle(0).version[:2]
-c = Circle (10)
-print 'A circle with a raduis of', c.redius
-print 'has an area of ', c.area()
+print u'Tutorial from Circuitous\N{trade mark sign}'
+print 'Circle class version: %d.%d' % Circle.version[:2]
+c = Circle(10)
+print 'A circle with a radius of', c.radius
+print 'has an area of', c.area()
 print
 
+
+## Academia ######################################################
+
+from random import random, seed  # should go at the top
+n = 100
+jenny = 8675309
+seed(jenny)
+
+print 'DARPA Grant Proposal to study the average area if random circles'
+print 'using Circuitous (TM) version %d.%d' % Circle.version[:2]
+print 'preliminary study of {n} random circles'.format(n=n)
+print "seeded with Jenny's number: {jenny}".format(jenny=jenny)
+
+#circles = []
+#for i in range(n):
+#    circle = Circle(random())
+#    circles.append(circle)
+#
+#area = []
+#for circle in circles:
+#    area.append(circle.area())
+
+circles = [Circle(random()) for i in range(n)]
+areas = [circle.area() for circle in circles]
+
+average_area = sum(areas)/n
+print 'The average area is %.1f' % average_area
+print
