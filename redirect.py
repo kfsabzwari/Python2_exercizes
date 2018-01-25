406e9e82a8d257dbf902b91610ff19873b6b5c52
kfsabzwari <35751192+kfsabzwari@users.noreply.github.com>
2018-01-25 15:08:14 -0800

    modified redirect.py to use StringIO on day 4

--- redirect.py
+++ redirect.py
@@ -9,6 +9,7 @@
 import os
 import sys
 import dis
+from StringIO import StringIO
 
 class RedirectStdout(object):
 
@@ -22,6 +23,29 @@ class RedirectStdout(object):
     def __exit__(self, exc_type, exc_inst, exc_tb):
         sys.stdout = self.orig_stdout
 
+class Ignore(object):
+
+    def __init__(self, exc_type):
+        self.exc_type = exc_type
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_inst, exc_tb):
+        if exc_type is not None and issubclass(exc_type, self.exc_type):
+            return True
+
+class Closing(object):
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __enter__(self):
+        return self.obj
+
+    def __exit__(self, exc_type, exc_inst, exc_tb):
+        self.obj.close()
+
 def show_family(lastname, first_names):
     "Display the family members in a nice tabular format."
     print 'The %s Family' % lastname
@@ -34,44 +58,14 @@ def show_family(lastname, first_names):
 with RedirectStdout(sys.stderr):
     show_family('Simpsons', ['Homer', 'Marge', 'Bart', 'Lisa', 'Maggie'])
 
-orig_stdout = sys.stdout
-sys.stdout = sys.stderr
-try:
-    show_family('Simpsons', ['Homer', 'Marge', 'Bart', 'Lisa', 'Maggie'])
-finally:
-    sys.stdout = orig_stdout
-
-f = open('show_family.help.txt', 'w')
-try:
-    orig_stdout = sys.stdout
-    sys.stdout = f
-    try:
+with Closing(StringIO()) as s:
+    with RedirectStdout(s):
         help(show_family)
-    finally:
-        sys.stdout = orig_stdout
-finally:
-    f.close()
-f = open('show_family.help.txt')
-try:
-    s = f.read()
-    print s.upper()
-finally:
-    f.close()
-try:
-    os.remove('show_family.help.txt')
-except OSError:
-    pass
+    print s.getvalue().upper()
 
-f = open('dis.txt', 'w')
-try:
-    orig_stdout = sys.stdout
-    sys.stdout = f
-    try:
+with open('dis.txt', 'w') as f:
+    with RedirectStdout(f):
         dis.dis(show_family)
-    finally:
-        sys.stdout = orig_stdout
-finally:
-    f.close()
 
 print 'DONE'
 
