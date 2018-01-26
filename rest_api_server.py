e8baaa114d986b94b72ba195817086471afca383
kfsabzwari <35751192+kfsabzwari@users.noreply.github.com>
2018-01-25 17:33:00 -0800

    updated rest_api_server for web on day 4

--- rest_api_server.py
+++ rest_api_server.py
@@ -1,5 +1,7 @@
 """ Goal: Learn to build small web apps and REST APIs.
 
+# https://bottlepy.org/
+
 Balancer/manager:   Apache, Ngninx, HAProxy
 
 Webserver:          gunicorn, uwsgi, cherrypy
@@ -18,6 +20,9 @@ Application:        algebra.py      <== Standalone Python Package
 
 from notes.bottle import run, get, response, request
 import time
+import subprocess
+import algebra
+import monkey_patch
 
 @get('/')
 def welcome():
@@ -33,5 +38,24 @@ def time_microservice():
     response.content_type = 'text/plain'
     return time.ctime()
 
+@get('/interfaces')
+def netstat_service():
+    response.content_type = 'text/plain'
+    return subprocess.check_output(['netstat', '-i'])
+
+@get('/circle/area')
+def circle_area_microservice():
+    try:
+        radius = float(request.query.get('radius', '0'))
+    except ValueError:
+        radius = 0
+    area = algebra.area(radius)
+    return dict(service=request.path, radius=radius, area=area)
+
 if __name__ == '__main__':
     run(host='localhost', port=5050, debug=True)
+    # $ python -m pip install gunicorn
+    # run(host='localhost', port=5055, server='gunicorn', workers=4)
+
+
+
